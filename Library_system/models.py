73a9c408b6bd29cb23ce14e8fec3a4047114dd09
KWojcik243 <63014344+KWojcik243@users.noreply.github.com>
2022-07-28 14:06:48 +0200

    User model

--- Library_system/models.py
+++ Library_system/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    # id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=40)
+    surname = models.CharField(max_length=40)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+    access_type = models.BooleanField()
+    
+    def __str__(self):
+        return self.name + ' ' + self.surname
+
